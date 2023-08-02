5385c27f96ab6e732699c7d7738ec6d7ba9382b1
dannykay62 <danielkay62@gmail.com>
2023-08-02 02:44:32 +0100

    Fixed the index

---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" route /status on the object app_views that returns a JSON"""
+
+
+from flask import jsonify
+import models
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+@app_views.route('/status', strict_slashes=False)
+def view_stats():
+    """gets number of each object by type"""
+
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
